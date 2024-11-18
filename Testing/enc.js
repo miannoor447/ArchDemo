f601a742a57e1cd6864b8f5e78ee867da671ed05
Aashir-Adnan <aashiradnan99@gmail.com>
2024-11-18 13:26:14 +0500

    nvm im dumb

--- Testing/enc.js
+++ Testing/enc.js
@@ -8,7 +8,7 @@ const EncryptedPayload = {
     "phoneNo" : "03090000323"
 }
 const object = {
-  EncryptedPayload : "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiSGFtbWFkIEthbXJhbiIsImVtYWlsIjoiYnNjZTIwMDI5QGl0dS5lZHUucGsiLCJwYXNzd29yZCI6Inh5ejEyMyIsInBob25lTm8iOiIwMzA5MDAwMDMyMyIsImlhdCI6MTczMTkxNzc1OX0.AwEgUcKJ0iTtNofpz3L6wM_9z_JU0yN8w9NkPOmUzQw",
+  EncryptedPayload : encryptObjectWithJWT( EncryptedPayload, "0c6693ac-a2ee-11" + process.env.SECRET_KEY),
   EncryptionDetails : {
     "platformName" : "Postman",
     "platformVersion" : "1.0"
