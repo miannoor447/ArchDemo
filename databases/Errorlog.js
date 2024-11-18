afa07838a8faa2fd720daf81eea616fbd9db455e
Aashir-Adnan <aashiradnan99@gmail.com>
2024-11-18 13:34:25 +0500

    nvm im dumb

--- databases/Errorlog.js
+++ databases/Errorlog.js
@@ -9,8 +9,8 @@ async function LogError(req,res,fileName,error, SCC){
     const [currentDateString,currentTimeString,currentDateTime] = getDateTime();;
     const Status="Active"
     console.log("error:::",error)
-    const query = "INSERT INTO errorlog (ErrorMessage, fileName, SCC, createdAt, updatedAt, Status) VALUES (?,?,?,?,?)";
-    const values = [error.toString(), fileName, SCC, currentDateTime,currentDateTime, Status];
+    const query = "INSERT INTO errorlog (ErrorMessage, fileName, createdAt, updatedAt, Status, scc) VALUES (?,?,?,?,?,?)";
+    const values = [error.toString(), fileName, currentDateTime,currentDateTime, Status, SCC];
     const results = await executeQuery(res, query, values, connection);
     const response = { status: 500, message: `${fileName} Error Logged: ${error}`, payload:results };
     return sendResponse(res, response.status, response.message, response.payload);
