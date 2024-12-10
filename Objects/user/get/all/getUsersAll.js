cc9ab84b7a9addd65cf9e2f1e630fa52bfd6b0bb
hamadkamran <hammad.kamran@granjur.com>
2024-12-10 16:06:02 +0500

    updated get api quereis

--- Objects/user/get/all/getUsersAll.js
+++ Objects/user/get/all/getUsersAll.js
@@ -30,7 +30,7 @@ global.ListUsersAll_object = {
             "apiInfo": {
               "query": {
                 "queryNature": "SELECT",
-                "queryPayload": "SELECT user_id AS id, users.* FROM users where status = 'active'",
+                "queryPayload": "SELECT user_id AS id,first_name AS personalInformation_firstName,last_name AS personalInformation_lastName,email AS personalInformation_email, users.* FROM users where status = 'active'",
                 "database": "projectDB"
               },
               "utilityFunctions": {
